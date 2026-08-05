c7789253ec5835e2f55bc0bbe591d194a9c54223
chichi <uhae451@gmail.com>
2026-08-05 16:57:20 +0900

    미션D 완료

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,31 @@ public class Main {
     public static void main(String[] args) {
 
         String customerName = "김현수";
-        int price = 15000;
+        int price = 1500;
         int quantity  = 6;
         boolean isMember = false;
-        int cash = 10000;
 
         // 미션 A
         int total = price * quantity;
+
+        // 미션 B
+        boolean isDiscountTarget = isMember || quantity >= 5;
+        if (isDiscountTarget) {
+            total -= (int) (total * 0.1);
+        }
+
+        int points = (int) (total * 0.05);
+        boolean isEligibleForGift = points >= 100;
+
+        // 미션 D
+        int cash = 10000;
+        int change = cash - total;
+        System.out.println("" +
+                customerName + "님의 영수증\n" +
+                "총 결제금액: " + total +"\n"+
+                "적립 포인트: " + points + "점\n"+
+                "사은품 대상: " + isEligibleForGift +"\n"+
+                "거스름돈: "+ change);
 
     }
 }
